58489915645ae5d4d51b2b07ea95b73fd8cd78dc
PKostecki <zreth99@gmail.com>
2022-06-14 17:52:12 +0200

    Heroku set-up

--- game/settings.py
+++ game/settings.py
@@ -14,11 +14,15 @@ from pathlib import Path
 import os
 import dotenv
 import sys
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# To have folder with apps
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+
+# Heroku set
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
