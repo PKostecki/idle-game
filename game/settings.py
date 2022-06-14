c102cc0ff787fb671bee9c47079a62fafcf2fdd5
PKostecki <zreth99@gmail.com>
2022-06-14 18:37:39 +0200

    Added CSRF_TRUSTED_ORIGINS

--- game/settings.py
+++ game/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
-
+CSRF_TRUSTED_ORIGINS = ['https://*.https://my-idle-game.herokuapp.com/', 'https://*.127.0.0.1']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
