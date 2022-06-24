8ee40c2b877f05a2cf050904dc1432e40289774e
PKostecki <zreth99@gmail.com>
2022-06-24 03:31:33 +0200

    Added user-register page

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from apps.users import views
 
 urlpatterns = [path("", views.dashboard, name="dashboard"),
-               path('accounts/', include('django.contrib.auth.urls'))]
+               path('accounts/', include('django.contrib.auth.urls')),
+               path("register", views.register, name="register")]
